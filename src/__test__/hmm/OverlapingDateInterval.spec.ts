6ccc40b4bf8fa07b3556be34d4d9c5061a7d170c
MATEUSZ EIDING <eidingmateusz@gmail.com>
2026-01-26 23:53:12 +0100

    Remove unneded files

--- src/__test__/hmm/OverlapingDateInterval.spec.ts
+++
@@ -1,3 +0,0 @@
-import overlapingDateInterval from "../../hmmm/OverlapingDateInterval";
-
-overlapingDateInterval([new Date("2025-03-01"), 6], [new Date("2025-03-03"), 9]);
--- src/sorting/QuickSort.ts
+++ src/sorting/QuickSort.ts
@@ -1,34 +1,31 @@
 export function quickSort<T>(arr: T[]) {
-    qs(arr, 0, arr.length - 1);
+	qs(arr, 0, arr.length - 1);
 }
 
 function qs<T>(arr: T[], lo: number, hi: number) {
-    if (lo >= hi) {
-        return;
-    }
+	if (lo >= hi) {
+		return;
+	}
 
-    const pivot = partition(arr, lo, hi);
+	const pivot = partition(arr, lo, hi);
 
-    qs(arr, lo, pivot - 1);
-    qs(arr, pivot + 1, hi);
+	qs(arr, lo, pivot - 1);
+	qs(arr, pivot + 1, hi);
 }
 
 function partition<T>(arr: T[], lo: number, hi: number) {
-    const pivot = arr[hi];
+	const pivot = arr[hi];
 
-    let idx = lo - 1;
-    for (let i = lo; i < hi; i++) {
-        if (arr[i] <= pivot) {
-            idx++;
-            const tmp= arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = tmp;
-        }
-    }
+	let idx = lo - 1;
+	for (let i = lo; i < hi; i++) {
+		if (arr[i] <= pivot) {
+			idx++;
+			[arr[i], arr[idx]] = [arr[idx], arr[i]];
+		}
+	}
 
-    idx++;
-    arr[hi] = arr[idx];
-    arr[idx] = pivot;
+	idx++;
+	[arr[hi], arr[idx]] = [arr[idx], arr[hi]];
 
-    return idx;
+	return idx;
 }
